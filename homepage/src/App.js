08a238bab82939838468d5c0584c01b62e1b2871
Diana Avila <davilam3@est.ups.edu.ec>
2025-11-07 14:28:46 -0500

    router

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,11 +1,15 @@
+// src/App.tsx
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Acciones from './componentes/Acciones';
 import PerfilPage from './componentes/PerfilPage';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/perfil">Perfil</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Acciones />} />
         <Route path="/perfil" element={<PerfilPage />} />
